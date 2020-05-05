337e7fe4ec8743896ccb966f73ba7a47c31bc49d
Manthan Gupta <manthangupta109@gmail.com>
2020-05-05 22:39:34 +0530

    screen size

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -5,7 +5,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'dart:async';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -27,21 +27,11 @@ class _HomeState extends State<Home> {
     }
   }
 
-  Size screenSize(BuildContext context) {
-    return MediaQuery.of(context).size;
-  }
-
-  double screenHeight(BuildContext context, {double dividedBy = 1}) {
-    return screenSize(context).height / dividedBy;
-  }
-
-  double screenWidth(BuildContext context, {double dividedBy = 1}) {
-    return screenSize(context).width / dividedBy;
-  }
-
-
   @override
   Widget build(BuildContext context) {
+    final dev_height =
+        MediaQuery.of(context).size.height; //height of device screen
+    final dev_width = MediaQuery.of(context).size.width;
     final Email email = Email(
       body: 'We Love Your Profile! Contact us',
       subject: 'Hiring',
@@ -54,10 +44,6 @@ class _HomeState extends State<Home> {
     const String medium = "https://medium.com/@manthangupta109";
     const String github = "https://github.com/Manthan109";
 
-    var screenSize = MediaQuery.of(context).size;
-    var width = screenSize.width;
-    var height = screenSize.height;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -69,44 +55,6 @@ class _HomeState extends State<Home> {
         ),
         elevation: 0.0,
         backgroundColor: const Color(0xFF041E40),
-        actions: <Widget>[
-          ButtonBar(
-            children: <Widget>[
-              FlatButton(
-                child: Text(
-                  "01. Home",
-                  style: GoogleFonts.robotoMono(
-                      color: const Color(0xFF39CEC0), fontSize: 12.0),
-                ),
-                onPressed: () {},
-              ),
-              FlatButton(
-                child: Text(
-                  "02. About",
-                  style: GoogleFonts.robotoMono(
-                      color: const Color(0xFF39CEC0), fontSize: 12.0),
-                ),
-                onPressed: () => print("Null"),
-              ),
-              FlatButton(
-                child: Text(
-                  "03. Works",
-                  style: GoogleFonts.robotoMono(
-                      color: const Color(0xFF39CEC0), fontSize: 12.0),
-                ),
-                onPressed: () => print("Null"),
-              ),
-              FlatButton(
-                child: Text(
-                  "04. Contact",
-                  style: GoogleFonts.robotoMono(
-                      color: const Color(0xFF39CEC0), fontSize: 12.0),
-                ),
-                onPressed: () => print("Null"),
-              )
-            ],
-          )
-        ],
       ),
       body: Container(
         decoration: BoxDecoration(color: const Color(0xFF041E40)),
@@ -116,155 +64,163 @@ class _HomeState extends State<Home> {
               Column(
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.only(left: 20.0, top: 400.0),
-                    child: Column(
-                      children: <Widget>[
-                        GFIconButton(
-                          color: const Color(0xFF041E40),
-                          shape: GFIconButtonShape.circle,
-                          onPressed: () => setState(() {
-                            _launched = _launchInBrowser(github);
-                          }),
-                          icon: Icon(
-                            FontAwesomeIcons.github,
+                    padding: EdgeInsets.only(left: 20.0, top: dev_height*0.45),
+                    child: Expanded(
+                      child: Column(
+                        children: <Widget>[
+                          GFIconButton(
+                            color: const Color(0xFF041E40),
+                            shape: GFIconButtonShape.circle,
+                            onPressed: () => setState(() {
+                              _launched = _launchInBrowser(github);
+                            }),
+                            icon: Icon(
+                              FontAwesomeIcons.github,
+                              color: const Color(0xFFCCCCFF),
+                            ),
+                          ),
+                          GFIconButton(
+                            color: const Color(0xFF041E40),
+                            shape: GFIconButtonShape.circle,
+                            onPressed: () => setState(() {
+                              _launched = _launchInBrowser(linkedin);
+                            }),
+                            icon: Icon(
+                              FontAwesomeIcons.linkedin,
+                              color: const Color(0xFFCCCCFF),
+                            ),
+                          ),
+                          GFIconButton(
+                            color: const Color(0xFF041E40),
+                            shape: GFIconButtonShape.circle,
+                            onPressed: () => setState(() {
+                              _launched = _launchInBrowser(medium);
+                            }),
+                            icon: Icon(
+                              FontAwesomeIcons.medium,
+                              color: const Color(0xFFCCCCFF),
+                            ),
+                          ),
+                          Container(
+                            width: 1.0,
+                            height: 90.0,
                             color: const Color(0xFFCCCCFF),
                           ),
-                        ),
-                        GFIconButton(
-                          color: const Color(0xFF041E40),
-                          shape: GFIconButtonShape.circle,
-                          onPressed: () => setState(() {
-                            _launched = _launchInBrowser(linkedin);
-                          }),
-                          icon: Icon(
-                            FontAwesomeIcons.linkedin,
-                            color: const Color(0xFFCCCCFF),
-                          ),
-                        ),
-                        GFIconButton(
-                          color: const Color(0xFF041E40),
-                          shape: GFIconButtonShape.circle,
-                          onPressed: () => setState(() {
-                            _launched = _launchInBrowser(medium);
-                          }),
-                          icon: Icon(
-                            FontAwesomeIcons.medium,
-                            color: const Color(0xFFCCCCFF),
-                          ),
-                        ),
-                        Container(
-                          width: 1.0,
-                          height: 90.0,
-                          color: const Color(0xFFCCCCFF),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ],
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 200.0, top: 100.0),
+                padding: EdgeInsets.only(
+                    left: dev_width * 0.1, top: dev_height * 0.15),
                 child: Container(
-                  height:550.0,
-                  width: 800.0,
+                  height: dev_height * 0.65,
+                  width: dev_width * 0.525,
                   child: Card(
                     color: const Color(0xFF041E40),
                     elevation: 0.0,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Row(children: <Widget>[
-                          Text(
-                            "Hi There!",
-                            style: GoogleFonts.robotoMono(color: Colors.white),
-                          ),
-                        ]),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                          child: Row(
-                            children: <Widget>[
-                              Text(
-                                "I'm Manthan",
-                                style: GoogleFonts.roboto(
-                                    color: const Color(0xFF44EFB7),
-                                    fontSize: 60.0,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
+                    child: Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Row(children: <Widget>[
+                            Text(
+                              "Hi There!",
+                              style:
+                                  GoogleFonts.robotoMono(color: Colors.white),
+                            ),
+                          ]),
+                          Padding(
+                            padding: EdgeInsets.only(top: dev_height * 0.011),
+                            child: Row(
                               children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 40.0),
-                                  child: Text(
-                                    "An undergraduate in sophomore year at Bennett University for Computer Science \nEngineering with experience in App Development, Cloud Technology and Web Development.\nI'm passionate about Technology and learning new things everyday.",
-                                    textAlign: TextAlign.left,
-                                    style: GoogleFonts.robotoMono(
-                                        color: Colors.white, height: 2.0),
-                                  ),
+                                Text(
+                                  "I'm Manthan",
+                                  style: GoogleFonts.roboto(
+                                      color: const Color(0xFF44EFB7),
+                                      fontSize: dev_width * 0.05,
+                                      fontWeight: FontWeight.bold),
                                 ),
                               ],
                             ),
-                          ],
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10.0, left: 0.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
+                          ),
+                          Row(
                             children: <Widget>[
-                              ButtonBar(
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 8.0, right: 20.0),
-                                    child: GFButton(
-                                      position: GFPosition.start,
-                                      type: GFButtonType.outline,
-                                      onPressed: () => setState(() {
-                                        _launched = _launchInBrowser(resume);
-                                      }),
-                                      text: "Resume",
-                                      textColor: const Color(0xFF39CEC0),
-                                      borderSide: BorderSide(
-                                          color: const Color(0xFF39CEC0),
-                                          width: 2.0),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 40.0),
+                                      child: AutoSizeText(
+                                        "A sophomore at Bennett University for Computer Science \nEngineering with experience in App Development, Cloud Technology and Web Development.\nI'm passionate about Technology and learning new things everyday.",
+                                        textAlign: TextAlign.left,
+                                        style: GoogleFonts.robotoMono(
+                                            color: Colors.white, height: 2.0),
+                                      ),
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 8.0, right: 20.0),
-                                    child: GFButton(
-                                      position: GFPosition.start,
-                                      type: GFButtonType.outline,
-                                      onPressed: () => setState(() async {
-                                        await FlutterEmailSender.send(email);
-                                      }),
-                                      text: "Hire Me",
-                                      textColor: const Color(0xFF39CEC0),
-                                      borderSide: BorderSide(
-                                          color: const Color(0xFF39CEC0),
-                                          width: 2.0),
-                                    ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
                             ],
                           ),
-                        )
-                      ],
+                          Padding(
+                            padding: EdgeInsets.only(top: 10.0, left: 0.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                ButtonBar(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 8.0, right: 20.0),
+                                      child: GFButton(
+                                        position: GFPosition.start,
+                                        type: GFButtonType.outline,
+                                        onPressed: () => setState(() {
+                                          _launched = _launchInBrowser(resume);
+                                        }),
+                                        text: "Resume",
+                                        textColor: const Color(0xFF39CEC0),
+                                        borderSide: BorderSide(
+                                            color: const Color(0xFF39CEC0),
+                                            width: 2.0),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 8.0, right: 20.0),
+                                      child: GFButton(
+                                        position: GFPosition.start,
+                                        type: GFButtonType.outline,
+                                        onPressed: () => setState(() async {
+                                          await FlutterEmailSender.send(email);
+                                        }),
+                                        text: "Hire Me",
+                                        textColor: const Color(0xFF39CEC0),
+                                        borderSide: BorderSide(
+                                            color: const Color(0xFF39CEC0),
+                                            width: 2.0),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
               Container(
-                height: 500.0,
-                width: 450.0,
+                height: dev_height * 0.71,
+                width: dev_width * 0.25,
                 child: Padding(
-                  padding: const EdgeInsets.all(20.0),
+                  padding: EdgeInsets.all(20),
                   child: Image.asset("assets/dev.png"),
                 ),
               )
